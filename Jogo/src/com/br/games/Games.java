5fb16a7065769612930ae07102d22a15466d8cc5
Claudionor Pontes <claudionor.pontes@outlook.com>
2022-06-21 13:12:09 -0300

    Jogo

---
+++ Jogo/src/com/br/games/Games.java
@@ -0,0 +1,17 @@
+package com.br.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeteerrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+public class Games {
+
+}
---
+++ Jogo/src/com/br/games/GamesI.java
@@ -0,0 +1,18 @@
+package com.br.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeteerrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeteerrosApplication.class, args);
+	}
+
+}
+
+
+public class GamesI {
+
+}
